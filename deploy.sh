54f914a67b94092e8e74cde234af96b0e0eaa17c
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-06-03 17:30:47 +0200

    ONLY 1 PAT LEFT!!!

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TAR_FILE='kkrol1.tar.gz'
+COMPRESED_FILE='kkrol.tgz'
 
-tar -cvzf $TAR_FILE *
-scp -i ~/.ssh/id_rsa $TAR_FILE krol_1197262@spk-ssh.if.uj.edu.pl:/home/krol_1197262
-ssh -i ~/.ssh/id_rsa krol_1197262@spk-ssh.if.uj.edu.pl "java -jar /scratch/uforamus/ChessTestClient/client.jar 172.30.24.15 $TAR_FILE"
+tar -zcvf $COMPRESED_FILE *
+scp -i ~/.ssh/id_rsa $COMPRESED_FILE krol_1197262@spk-ssh.if.uj.edu.pl:/home/krol_1197262
+ssh -i ~/.ssh/id_rsa krol_1197262@spk-ssh.if.uj.edu.pl "java -jar /scratch/uforamus/ChessTestClient/client.jar 172.30.24.15 $COMPRESED_FILE"
